d66fc4942fd565c85894bf8a487067809e3237d9
Daniele Barbaro <barbaro.daniele@gmail.com>
2024-06-16 13:19:35 +0200

    L10 and L11 package conversion

---
+++ rector.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\SetList;
+use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector;
+use RectorLaravel\Set\LaravelSetList;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__.'/src',
+        __DIR__.'/tests',
+    ])
+    ->withPhpSets()
+    ->withSets([
+        LaravelSetList::LARAVEL_100,
+        LaravelSetList::LARAVEL_110,
+
+        SetList::PHP_83,
+        SetList::PHP_82,
+        SetList::CODING_STYLE,
+        SetList::CODE_QUALITY,
+        SetList::TYPE_DECLARATION,
+
+        \Rector\Set\ValueObject\LevelSetList::UP_TO_PHP_82,
+    ])
+    ->withRules([
+        AddVoidReturnTypeWhereNoReturnRector::class,
+    ]);
