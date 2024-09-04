3b4594eb474649df2294c2bbb0f5c6c38aa274e2
dualklip <dualklip@gmail.com>
2024-09-04 16:52:59 +0200

    Add VAT validation messages for Spanish and Italian
    Introduced new language files for VAT validation messages in Spanish and Italian. These include messages for valid VAT numbers, correct VAT number formatting, and existence checks.

---
+++ resources/lang/es/validation.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'vat_number' => 'El :attribute debe ser un número de IVA válido.',
+    'vat_number_format' => 'El :attribute debe escribirse en un formato numérico válido {country_name}{vat_number}.',
+    'vat_number_exist' => 'Número de IVA :attribute no existe.',
+];
---
+++ resources/lang/it/validation.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'vat_number' => 'Il :attribute deve essere un numero di partita IVA valido.',
+    'vat_number_format' => 'Il :attribute deve essere scritto in un formato numerico valido {nome_paese}{numero_iva}.',
+    'vat_number_exist' => 'Il numero di partita IVA :attribute non esiste.',
+];
