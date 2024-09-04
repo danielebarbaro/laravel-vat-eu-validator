2cfd842110250b971f394b90d9832e13d5b2fe4a
dualklip <dualklip@gmail.com>
2024-09-04 17:00:27 +0200

    Add VAT number validation messages
    Include messages for VAT number validation: valid format, correct writing format, and existence check. This extends the validation rules and provides more detailed feedback for VAT number-related fields.

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -1,1 +1,6 @@
 <?php
+return [
+    'vat_number' => 'The :attribute must be a valid VAT number.',
+    'vat_number_format' => 'The :attribute must be write in a valid number format {country_name}{vat_number}.',
+    'vat_number_exist' => 'VAT number :attribute not exist.',
+];
