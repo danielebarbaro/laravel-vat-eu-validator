3f92c42147a1906cc575085b51857a1b840655ba
bestmomo <colorstonic@free.fr>
2026-08-10 17:07:44 +0200

    French translations

---
+++ resources/lang/fr/validation.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'vat_number' => 'Le champ :attribute doit être un numéro de TVA valide.',
+    'vat_number_format' => 'Le champ :attribute doit être écrit dans un format valide {nom_pays}{numéro_tva}.',
+    'vat_number_exist' => 'Le numéro de TVA :attribute n\'existe pas.',
+];
